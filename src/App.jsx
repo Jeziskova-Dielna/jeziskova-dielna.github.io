53fd45c5ce9054ba4a18efd7d895ade38549387a
SlenkyDev <slenky@protonmail.com>
2022-11-13 18:49:29 +0100

    oopsie

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
                 <p className="text-4xl md:text-5xl font-extralight hover:font-extrabold cursor-default select-none bg-white py-6 rounded-lg hover:rounded-full md:w-96 w-72 text-center drop-shadow-[0_15px_35px_rgba(255,255,255,0.25)] hover:-translate-y-3 transition-all duration-100 md:hover:w-[35rem] md:hover:h-[35rem] hover:w-[20rem] hover:h-[20rem] hover:absolute flex items-center justify-center">
                     <Countdown
                         className="text-dark"
-                        date={Date.parse("20 Nov 2022 19:00:00 GMT+1")}
+                        date={Date.parse("25 Nov 2022 19:00:00 GMT+1")}
                     />
                 </p>
             </div>

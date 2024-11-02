d97f0bb1f62a36db14223cde43ceb8f898d30e85
oliminator <oliverplassiard@proton.me>
2024-11-02 12:45:25 +0100

    dfhrtzh

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,26 @@ import Snowfall from "react-snowfall";
 
 function App() {
     return (
-        <main className="px-4  bg-gradient-to-r  from-dark to-black select-none">
-            <Snowfall />
+        <main className="relative bg-gradient-to-r from-blue to-black select-none">
+            <Snowfall color="white" snowflakeCount={100} />
             <div className="text-white text-center h-screen flex justify-center items-center flex-col">
-                {" "}
                 <a
                     href="https://discord.com/events/768806954205184002/1040671696803332197"
                     target="_blank"
+                    rel="noopener noreferrer"
+                    className="pointer-events-auto"
                 >
-                    {" "}
-                    <h1 className="font-black text-6xl my-6 pointer-events-none cursor-pointer select-none md:mb-2 mb-24">
+                    <h1 className="font-black text-6xl my-6 md:mb-2 mb-24">
                         Ježiškova Dielňa
                     </h1>
                 </a>
-                <span className="mb-8 text-xl font-light max-w-3xl font-[Berkshire_Swash, cursive']">
+                <span className="mb-8 text-xl font-light max-w-3xl font-[Berkshire_Swash, cursive'] text-white">
                     U nás na Ježiškovej dielni každoročne počas Vianoc
                     organizujeme rôzne súťaže, komunitné eventy a viac!
                 </span>
                 <a
                     href="https://bit.ly/jeziskova-dielna"
-                    className="z-50 bg-white/90 hover:bg-white transition-all px-12 py-3 rounded-xl text-black text-xl"
+                    className="z-50 bg-red-600 hover:bg-red-500 transition-all px-12 py-3 rounded-xl text-white font-bold text-xl shadow-lg animate-bounce"
                 >
                     Pripoj sa!
                 </a>
@@ -32,6 +32,7 @@ function App() {
                         date={Date.parse("25 Nov 2024 19:00:00 GMT+1")}
                     />
                 </p>
+                {/* Snow at the bottom */}
             </div>
         </main>
     );

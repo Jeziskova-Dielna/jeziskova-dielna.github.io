124370a1fed57eeafa94b83850d0da603dd264e8
oliminator <oliverplassiard@proton.me>
2024-11-07 22:06:53 +0100

    nova stranka

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,49 @@ import "./index.css";
 import Countdown from "react-countdown";
 import Snowfall from "react-snowfall";
 
+// Import images manually
+import img1 from "./imgs/1.png";
+import img2 from "./imgs/2.png";
+import img3 from "./imgs/3.png";
+import img4 from "./imgs/4.png";
+import img5 from "./imgs/5.png";
+import img6 from "./imgs/6.png";
+
+const images = [img1, img2, img3, img4, img5, img6];
+
+function ImageSlider() {
+    return (
+        <div className="slider mb-12">
+            <div className="slide-track">
+                {images.concat(images).map((img, index) => (
+                    <img
+                        key={index}
+                        src={img}
+                        alt={`slide-${index}`}
+                        className="slide"
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
 function App() {
     return (
-        <main className="relative bg-gradient-to-r from-blue to-black select-none">
+        <main className="relative bg-gradient-to-r from-blue-900 to-black select-none overflow-hidden">
             <Snowfall color="white" snowflakeCount={100} />
-            <div className="text-white text-center h-screen flex justify-center items-center flex-col">
+            <div className="text-white text-center h-screen flex items-center flex-col">
                 <a
                     href="https://discord.com/events/768806954205184002/1040671696803332197"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="pointer-events-auto"
                 >
-                    <h1 className="font-black text-6xl my-6 md:mb-2 mb-24">
+                    <h1 className="font-black text-6xl my-6 md:mb-2 mb-24 drop-shadow-lg mt-24">
                         Ježiškova Dielňa
                     </h1>
                 </a>
-                <span className="mb-8 text-xl font-light max-w-3xl font-[Berkshire_Swash, cursive'] text-white">
+                <span className="mb-8 text-xl font-light max-w-3xl font-[Berkshire_Swash, cursive] text-white drop-shadow-md">
                     U nás na Ježiškovej dielni každoročne počas Vianoc
                     organizujeme rôzne súťaže, komunitné eventy a viac!
                 </span>
@@ -27,13 +54,13 @@ function App() {
                 >
                     Pripoj sa!
                 </a>
-                <p className="text-[15vw] text-white/20 font-extrabold absolute">
+                <p className="text-[15vw] text-white/20 font-extrabold absolute top-1/3 transform -translate-y-1/2 w-full flex justify-center drop-shadow-lg">
                     <Countdown
                         date={Date.parse("25 Nov 2024 19:00:00 GMT+1")}
                     />
                 </p>
-                {/* Snow at the bottom */}
             </div>
+            <ImageSlider />
         </main>
     );
 }

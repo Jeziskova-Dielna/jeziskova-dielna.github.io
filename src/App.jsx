159406fb4cc6574c11839f316e68723332c30b3e
JopGamer <59136132+JopGamer@users.noreply.github.com>
2025-12-07 17:57:38 +0100

    Update countdown date to 24 Nov 2024

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ function App() {
                 </a>
                 <p className="text-[15vw] text-white/20 font-extrabold absolute top-1/3 transform -translate-y-1/2 w-full flex justify-center drop-shadow-lg">
                     <Countdown
-                        date={Date.parse("25 Nov 2024 19:00:00 GMT+1")}
+                        date={Date.parse("24 Nov 2024 19:00:00 GMT+1")}
                     />
                 </p>
             </div>
